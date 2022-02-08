d406c909d06d98f4532617d79711493622b100ff
McDinii <denisnechay@gmail.com>
2022-02-08 22:11:45 +0300

    collect static files

--- jobs/views.py
+++ jobs/views.py
@@ -4,5 +4,5 @@ from .models import Job
 
 def home(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'jobs/home.html', {'jobs': jobs})
 
